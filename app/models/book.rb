b9c2fe374cac42b9025dbeb349b4f72eb0f5ed49
charlesagyemang <micnkru@gmail.com>
2021-07-10 18:51:59 +0000

    initialize rails project wirh seeded db and raw seql queries

--- app/models/book.rb
+++ app/models/book.rb
@@ -14,5 +14,43 @@ class Book < ApplicationRecord
     res = self.find_by_sql _query
     res.first.custom
   end
+
+
+  def self.topics_stats
+    _query = """
+    SELECT json_agg(t) AS custom
+    FROM(
+      SELECT topic,
+        COUNT(topic) AS topic_count,
+        MIN(CAST(pages AS INT)) AS min_pg,
+        MAX(CAST(pages AS INT)) AS max_pg,
+        ROUND ( AVG(CAST(pages AS INT)), 2) AS avg_pages
+      FROM books
+      GROUP BY topic
+    )t;
+    """
+    res = self.find_by_sql _query
+    res.first.custom
+  end
+
+  def self.topics_stats_having
+    puts "================== START TIME:  #{Time.now()}  ====================="
+    _query = """
+    SELECT json_agg(t) AS custom
+    FROM(
+      SELECT authors.name, 
+        topic, 
+        COUNT(books.id) AS books_written
+      FROM authors
+          JOIN books ON books.author_id = authors.id
+      GROUP BY authors.name, topic
+      HAVING COUNT(books.id) > 5
+      ORDER BY COUNT(books.id) DESC
+    )t;
+    """
+    res = self.find_by_sql _query
+    puts "================== END TIME: #{Time.now()}  ====================="
+    res.first.custom
+  end
   
 end
